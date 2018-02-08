d2128fecc9e4848b66eb16ddf6eb9d25bf568e47
loveritsu929Home <loveritsu929@gmail.com>
2018-02-07 23:28:37 -0500

    complete

--- src/asg/A4.java
+++ src/asg/A4.java
@@ -63,11 +63,43 @@ public class A4 {
     	// 1 <= k <= D(A.size())
 
     	// You must fill in this routine
+    	Vector<Integer> target = new Vector<Integer>();
     	int n = A.size();
+    	
+    	if(n==1){
+    		//do nothing. empty target
+    		return target;
+    	}
+    	else if(n==2){
+    		//reversed
+    		target.add(A.elementAt(1));
+    		target.add(A.elementAt(0));
+    		return target;
+    	}
+    	else if(n==3){
+    		int k2 = k.intValue();
+    		if(k2==1){
+    			target.add(A.elementAt(1));
+    			target.add(A.elementAt(2));
+    			target.add(A.elementAt(0));
+    		}
+    		else if(k2==2){
+    			target.add(A.elementAt(2));
+    			target.add(A.elementAt(0));
+    			target.add(A.elementAt(1));
+    		}
+    		return target;
+    	}
+    	
+    	// n > 3
     	BigInteger case_one_size =BigInteger.valueOf(n-1).multiply(D(n-2));
     	BigInteger case_two_size =BigInteger.valueOf(n-1).multiply(D(n-1));
     	BigInteger group_size = D(n-2).add(D(n-1)); // size of each group of case1&2 for a particular i
-    	BigInteger group_number = k.mod(group_size).equals(BigInteger.ZERO) ? k.divide(group_size) : k.divide(group_size).add(BigInteger.ONE); // index of a group
+    	
+    	BigInteger group_number = k.mod(group_size).equals(BigInteger.ZERO) ? k.divide(group_size) : k.divide(group_size).add(BigInteger.ONE); // index of a group == i >= 1
+    	if(group_number.equals(BigInteger.ZERO))
+    		group_number = BigInteger.ONE;
+    	
     	BigInteger index_in_group = k.mod(group_size).equals(BigInteger.ZERO) ? group_size :  k.mod(group_size);  // index of kth assignment in a group; [0+1, group_size - 1 + 1]
     	
     	int caseNum = 1;
@@ -82,39 +114,47 @@ public class A4 {
     		index_in_case = index_in_group.subtract(D(n-2)); // index_in_case2 = index_in_group - sizeOf(case1)
     	}
     	
-    	Vector<Integer> target = new Vector<Integer>();
     	
-    	if(n==1){
-    		//do nothing. empty target
-    	}
-    	else if(n==2){
-    		//reversed
-    		target.add(A.elementAt(1));
-    		target.add(A.elementAt(0));
-    	}
-    	else{// n>2
+    	
+    		// n>3
     		
     		if(caseNum == 1)
     		{ 
+    			Vector<Integer> A1 = new Vector<Integer>(); // A1: (n-2) elts; handle books except [i] and [n]
+    			A1.addAll(A);
+    			A1.remove(n-1);
+    			A1.remove(group_number.subtract(BigInteger.ONE).intValue()); // group_number == i== with whom G[n] exchange the book
     			
+    			//System.out.println("A1:" + A1);
     			
+    			target = SelectAssignment(A1, index_in_case);
     			
+    			//System.out.println("target1:" + target);
     			
+    			target.add(group_number.subtract(BigInteger.ONE).intValue(), A.elementAt(n-1) ); // G[i] get B[n]
+    			target.add(A.elementAt(group_number.subtract(BigInteger.ONE).intValue()));		 // G[n] get B[i]
+    			
+    			//System.out.println("target2:" + target);
     		}
     		else{ // caseNum ==2
     			
+    			Vector<Integer> A2 = new Vector<Integer>();		// A2: (n-1) elts
+    			A2.addAll(A);
+    			A2.setElementAt(A2.elementAt(n-1), group_number.subtract(BigInteger.ONE).intValue());        // G[i] get B[n]
+    			A2.remove(n-1);									 
     			
-    			
+    			target = SelectAssignment(A2, index_in_case);
+    			target.add(A.elementAt(group_number.subtract(BigInteger.ONE).intValue()));		  // G[n] get B[i]
     			
     		}
     		
-    	}
     	
     	
     	
     	
     	
     	
+    		
     	
     	
     	return target;
@@ -125,8 +165,10 @@ public class A4 {
 
     public static void main(String[] args) {
     	// Mr Montmort tested his Assignments code for n=5 as follows.
-
-    	int size = 4;
+    	
+    	//System.out.println(D(20));
+    	
+    	int size = 5;
     	Vector<Integer> identity = new Vector<Integer>(size);
 		for (int i = 1; i <= size; i++)
 			identity.add(i);
@@ -135,10 +177,10 @@ public class A4 {
 			System.out.println(B);
 
 		// Next, Mr Montmort wants to test SelectAssignment as follows
-		
+		System.out.println("target:");
 		System.out.println(SelectAssignment(identity, new BigInteger("9")));
 		
-		/*
+		
 		size = 20;
 		identity = new Vector<Integer>(size);
 		for (int i = 1; i <= size; i++)
@@ -150,6 +192,6 @@ public class A4 {
 		identity.remove(size-1);
 		System.out.println(SelectAssignment(identity, new BigInteger("44750731559645106")));
 		System.out.println(SelectAssignment(identity, new BigInteger("44750731557645106")));
-		*/
+		
     }
 }
